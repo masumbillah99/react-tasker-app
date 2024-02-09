56220be46232e10185bf8963ffcca2e9f7d0e91f
Masum Billah <masumbillahyusuf1621@gmail.com>
2024-02-10 00:01:48 +0600

    task board ui clone in react from html template

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import "./App.css";
 import Footer from "./Footer";
 import HeroSection from "./HeroSection";
 import Navbar from "./Navbar";
+import TaskBoard from "./task/TaskBoard";
 
 function App() {
   return (
     <div className="bg-[#191D26] font-[Inter] text-white">
       <Navbar />
-      <div className="flex flex-col items-center justify-center">
+      <div className="flex flex-col max-w-screen-2xl mx-auto">
         <HeroSection />
+        <TaskBoard />
       </div>
       <Footer />
     </div>
